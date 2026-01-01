b1f609132cd4b8ba24d90729a37986a8ce157ef7
nguyenhuy-dev <nguyenhuy.se205@gmail.com>
2026-01-01 21:43:03 +0700

    learning: stream file with dir and copy commands

---
+++ stream-file/ncopy/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ncopy
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+        }
+
+        static void Test()
+        {
+            var source = @"D:\test\HelloWorld.txt";
+            var dist = @"D:\test\HelloWorld-copy.txt";
+
+            var buffer = new byte[256];
+            using var instream = File.OpenRead(source);
+            using var outstream = File.OpenWrite(dist);
+
+            int n = instream.Read(buffer, 0, buffer.Length);
+            while (n > 0)
+            {
+                Console.WriteLine(n.ToString());
+
+                outstream.Write(buffer, 0, n);
+
+                n = instream.Read(buffer, 0, buffer.Length);
+            }
+            
+            // instream.Close();
+            // outstream.Close(); //nếu không chịu đóng file out lại thì coi như chưa copy dữ liệu vô
+            // viết lệnh "using" trước var thì câu lệnh đóng sẽ tự động được gọi
+        }
+    }
+}
---
+++ stream-file/ndir/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ndir
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+        }
+
+        // Clone lệnh "dir" trong cmd bằng SYSTEM.IO
+        static void Test()
+        {
+            var path = "C:\\";
+
+            var dir = new DirectoryInfo(path);
+            var directories = dir.GetDirectories();
+            //var directories = Directory.GetDirectories(path);  // này chỉ là tên của các file, không gồm đủ info
+
+            foreach (var d in directories)
+                Console.WriteLine($"{d.LastWriteTime:MM/dd/yyyy} {d.LastWriteTime:HH:mm}    <DIR>   {d.Name}");
+
+            var files = dir.GetFiles();
+            foreach (var f in files)
+                Console.WriteLine($"{f.LastWriteTime:MM/dd/yyyy} {f.LastWriteTime:HH:mm}          {f.Length:#,###} {f.Name}");
+        }
+    }
+}
